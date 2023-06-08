949ccb87b92705c16d17e38d5d3784ca27e53b8e
Anand Patil <127176608+Anandpatil7026@users.noreply.github.com>
2023-06-08 12:35:36 +0530

    Create fork.c

---
+++ fork.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<unistd.h>
+
+int main()
+{
+    int loc=6;
+    int pid=fork();
+    if(pid==0)
+    {
+        printf("child process pid=%d\n",getpid());
+        printf("its parent process pid=%d\n",getpid());
+        loc++;
+    }
+else {
+    sleep(2);
+    printf("parent process pid=%d\n",getpid());
+    printf("its parent process pid=%d\n",getpid());
+}
+printf("loc=%d\n",loc);
+}
