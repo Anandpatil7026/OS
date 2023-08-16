49d12bd5a0f250c080f925ac484f5b4dfcc655ab
Anand Patil <anandpatil7026@gmail.com>
2023-08-16 21:43:10 +0530

    Update fork.c

--- fork.c
+++ fork.c
@@ -3,18 +3,20 @@
 
 int main()
 {
-    int loc=6;
-    int pid=fork();
-    if(pid==0)
+    pid_t pid;
+    int status;
+    pid=fork();
+    if(pid<0)
     {
-        printf("child process pid=%d\n",getpid());
-        printf("its parent process pid=%d\n",getpid());
-        loc++;
+        printf("Error:fork() failed\n");
+        return 1;
+    }else if(pid==0){
+        
+        printf("this is the child process with pid %d\n",getpid());
+        printf("this is the parent process pid %d\n",getpid());
+        execlp("/bin/ls","ls",NULL)
+            printf("this should not be printed if execlp() is successful \n");
+        return 0;
     }
 else {
-    sleep(2);
-    printf("parent process pid=%d\n",getpid());
-    printf("its parent process pid=%d\n",getpid());
-}
-printf("loc=%d\n",loc);
-}
+    printf("this is the parent process pid %d \n",getpid());
